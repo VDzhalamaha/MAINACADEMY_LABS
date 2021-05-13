2232657b898f24900cfa7f1da86faa49c09e0de9
Василий Джаламага <vdzhalamaha@gmail.com>
2021-05-13 19:56:00 +0300

    comm2

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lab1
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            string a = "Hello World!";
+            //var i = true;
+            //if (i) 
+            //{\\////
+                Console.WriteLine(a);
+            //}
+            Console.ReadLine();
+        }
+    }
+}
---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string aStr = Console.ReadLine();
+            int aInt = 1;
+            Console.WriteLine(aInt);
+            Console.ReadLine();
+           
+
+
+
+        }
+    }
+}
